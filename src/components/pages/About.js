cb4cf39251729721c5719c9d39cf11339a6cfd18
Ibra-K <ibrkhld@gmail.com>
2019-04-05 13:05:44 +0100

    Update code

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is the Todolist app v1.0.0. It is part of a React </p>
+    </React.Fragment>
+  )
+}
+
+
+export default About;
